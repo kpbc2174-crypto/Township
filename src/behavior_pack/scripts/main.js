7c5be14100b9700d7fe304dc8247a839606063f3
kpbc2174-crypto <kpbc2174@gmail.com>
2026-06-25 06:23:47 -0500

    Restore direct Township runtime entry

--- src/behavior_pack/scripts/main.js
+++ src/behavior_pack/scripts/main.js
@@ -1,5 +1,1 @@
-import { installTownshipTickingAreaRuntime } from "./core/ticking_area_runtime.js";
-
-installTownshipTickingAreaRuntime();
-
-import("./app/runtime.js");
+import "./app/runtime.js";
